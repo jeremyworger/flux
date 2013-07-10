12a4c30fabdf80c818f33b56a9fcea8d657e7c5b
Lester Hedges <lester.hedges@gmail.com>
2013-07-10 16:14:16 -0700

    Fixed typo in help message.

--- src/flux.cpp
+++ src/flux.cpp
@@ -323,7 +323,7 @@ void parseCommandLineArguments(int argc, char **argv, ostringstream &targetFileN
 			else
 			{
 				cerr << "Error: unknown command-line parameter " << argv[i] << endl;
-				cerr << "Run: for help run \"mc_charcoal -h\"" << endl;
+				cerr << "Run: for help run \"flux -h\"" << endl;
 				exit(1);
 			}
 
